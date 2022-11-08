53d9bd908868885b94057698515daba386bbe6d0
MSI\voquo_706 <voquochuy30112@gmail.com>
2022-11-08 17:09:00 +0700

    dsw

--- resources/views/reply-feedback.blade.php
+++ resources/views/reply-feedback.blade.php
@@ -4,19 +4,37 @@
         <div class="title font-weight-bold h1 text-center pt-5">
             Trả Lời phản Hồi
         </div>
+        <style>
+            .sender,
+            .reply-tile {
+                display: inline-block;
+            }
 
+            .sender-time {
+                margin-left: 87%;
+            }
+
+            .sender-content {
+                border: 1px #000 solid;
+                border-radius: 10px;
+                padding: 20px;
+            }
+        </style>
         <div class="sender-information">
-            <div class="sender-name">Nguyễn Văn A</div>
-            <div class="sender-phone">1234567890</div>
-            <div class="sender-mail">nguyenvana@gmail.com</div>
-            <div class="sender-time">10h22(10/07/2022)</div>
+            <div class="sender sender-name font-weight-bold">Nguyễn Văn A</div>
+            <div class="sender sender-mail">
+                <\nguyenvana@gmail.com />
+            </div>
+            <div class="sender sender-time">10h22(10/07/2022)</div>
+
             <div class="sender-content">Lorem ipsum dolor sit amet consectetur, adipisicing elit. In voluptas commodi repudiandae quidem exercitationem ex mollitia odit corporis cupiditate omnis nihil sint, id sed, nam doloremque assumenda voluptatum nisi rerum?</div>
 
         </div>
 
-        <div class="reply">
+        <div class="reply pt-5">
             <div class="reply-information-sender">
-                <span class="reply-name">Nhân Viên: Nguyễn Thi B</span>
+                <div class="reply-tile font-weight-bold">Trả lời</div>
+                <span class="reply-name ml-5">Nhân Viên: Nguyễn Thi B</span>
                 <span class="reply-time">đã trả lời vào ngày 10h25(10/07/2022)</span>
             </div>
 
